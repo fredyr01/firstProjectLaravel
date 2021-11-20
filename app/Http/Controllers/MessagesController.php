81882c864f61c300854a1e7ddf45549bf871cf03
fredyr01 <fredyrios01@gmail.com>
2021-11-20 11:57:52 -0500

    formularios y email

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MessagesController extends Controller
 {
     public function store(){
-        return request();
+        request()->validate([
+            'name' => 'required',
+            // 'email' => 'required|email'
+            //* o se puede pasar un array con las condiciones
+            'email' => ['required', 'email'],
+            'subject' => 'required',
+            'content' => 'required|min:3'   //* Tenga minimo 3 caracteres
+        ],[
+            'name.required' => 'Necesito tu nombre'
+        ]);
+
+        Mail::to('fredyrios01@gmail.com')->send(new MessageReceived);
+        return 'Mensaje enviado';
+
+
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,13 +3,36 @@
 @section('title','Contact')
 
 @section('content')
-    <h1>Contact</h1>
+    <h1>{{ __('Contact')}}</h1>
+    {{-- {{var_dump($errors->any())}}
+    Me muestra false si hay error o true si hay errores de validación --}}
+    {{-- @if ($errors->any()) --}}
+        {{-- {{ var_dump($errors->all()) }} --}}
+        {{-- Muestra array con los errores de validación --}}
+
+        {{-- For each que muestra todos los errores de validación --}}
+        {{-- <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul> --}}
+
+        {{-- Para mostrar el primer error
+        {{$errors->first('name')}} --}}
+        {{-- Para mostrarlo en cada input, se coloca en cada uno especificando el name --}}
+
+    {{-- @endif --}}
+
     <form method="POST" action="{{ route('contact') }}">
         @csrf
-        <input name="name" placeholder="Nombre ... " value="Fredy Rios"><br><br>
-        <input name="email" type="email" placeholder="Correo ... " value="fredyrios01@gmail.com"><br><br>
-        <input name="subject" placeholder="Asunto ... " value="Asunto de prueba"><br><br>
-        <textarea name="content" placeholder="Mensaje ... ">Mensaje de prueba</textarea><br><br>
-        <button>Enviar</button>
+        <input name="name" placeholder="Nombre ... " value="{{ old('name') }}"><br>
+        {!! $errors->first('name','<small>:message</small><br>') !!} <br>
+        <input name="email" type="email" placeholder="Correo ... " value="{{ old('email') }}"><br>
+        {!! $errors->first('email','<small>:message</small><br>') !!} <br>
+        <input name="subject" placeholder="Asunto ... " value="{{ old('subject') }}"><br>
+        {!! $errors->first('subject','<small>:message</small><br>') !!} <br>
+        <textarea name="content" placeholder="Mensaje ... ">{{ old('content') }}</textarea><br>
+        {!! $errors->first('content','<small>:message</small><br>') !!} <br>
+        <button>@lang('Send')</button>
     </form>
 @endsection
