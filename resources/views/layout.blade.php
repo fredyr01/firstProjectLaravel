2a83388f9949c77e96e4043448e38876898d80c4
fredyr01 <fredyrios01@gmail.com>
2021-11-27 11:54:33 -0500

    ultimando detalles con laravel mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Valor por defecto')</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js" defer></script>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <script src="{{mix('js/app.js')}}" defer></script>
     <style type="text/css">
-        .active a{
+        /* .active a{
             color: red;
             text-decoration: none;
-        }
+        } */
     </style>
 </head>
 <body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,10 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.browserSync('http://app.test')
+
+if(mix.inProduction()){
+    mix.version();
+}
